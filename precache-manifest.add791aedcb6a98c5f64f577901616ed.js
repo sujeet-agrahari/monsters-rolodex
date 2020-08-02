42cd56f7ef5efbc0c753ba338fb108531adf2d87
Sujeet Agrahari <sujeet.rgcsm@gmail.com>
2020-08-02 17:00:43 +0530

    Updates

---
+++ precache-manifest.add791aedcb6a98c5f64f577901616ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f14cf5cdd118f8a6309c0e5924cdad0f",
+    "url": "/monster-rolodox/index.html"
+  },
+  {
+    "revision": "7eb223497a8a74b01eec",
+    "url": "/monster-rolodox/static/css/main.54ed75ee.chunk.css"
+  },
+  {
+    "revision": "2b4c1199f1e002dcdb8b",
+    "url": "/monster-rolodox/static/js/2.e1eeb85d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodox/static/js/2.e1eeb85d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eb223497a8a74b01eec",
+    "url": "/monster-rolodox/static/js/main.641d7ea5.chunk.js"
+  },
+  {
+    "revision": "bb401f088731217aa852",
+    "url": "/monster-rolodox/static/js/runtime-main.646deca2.js"
+  }
+]);
